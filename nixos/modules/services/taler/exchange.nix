dcd4133eef990917676f8d9a2028aa8a13912e8b
Atemu <atemu.main@gmail.com>
2024-06-27 20:10:18 +0200

    exchange module cont

--- nixos/modules/services/taler/exchange.nix
+++ nixos/modules/services/taler/exchange.nix
@@ -1,7 +1,43 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+let
+  this = config.services.taler.exchange;
+  services = [ "httpd" ];
+  inherit (config.services.taler) configFile;
+in
 
 {
   options.services.taler.exchange = {
     enable = lib.mkEnableOption "the GNU Taler exchange";
+    package = lib.mkPackageOption pkgs "taler-exchange" { };
+    debug = lib.mkEnableOption "debug logging";
+  };
+
+  config = lib.mkIf this.enable {
+    systemd.slices.taler-exchange = {
+      description = "Slice for GNU taler exchange processes";
+      before = [ "slices.target" ];
+    };
+
+    systemd.services = lib.genAttrs (map (n: "taler-exchange-${n}") services) (name: {
+      serviceConfig = {
+        DynamicUser = true;
+        User = name;
+        ExecStart =
+          "${this.package}/bin/${name} -c ${configFile}" + lib.optionalString this.debug " -L debug"; # TODO as a list?
+        RuntimeDirectory = name;
+        # TODO more hardening
+        # PrivateTmp = "yes";
+        # PrivateDevices = "yes";
+        # ProtectSystem = "full";
+        # Slice = "taler-exchange.slice";
+      };
+      wantedBy = [ "multi-user.target" ]; # TODO slice?
+    });
   };
 }
