c7951d868d4341ac4a605532261da4398c681d48
Atemu <atemu.main@gmail.com>
2024-06-27 20:20:28 +0200

    basic config set that makes the service at least start

--- nixos/modules/services/taler/exchange.nix
+++ nixos/modules/services/taler/exchange.nix
@@ -39,5 +39,25 @@ in
       };
       wantedBy = [ "multi-user.target" ]; # TODO slice?
     });
+    services.taler.settings = {
+      exchange = {
+        # TODO these should be generated from high-level NixOS options
+        AML_THRESHOLD = "KUDOS:1000000";
+        MAX_KEYS_CACHING = "forever";
+        DB = "postgres";
+        MASTER_PUBLIC_KEY = "Q6KCV81R9T3SC41T5FCACC2D084ACVH5A25FH44S6M5WXWZAA8P0";
+        # WIRE_RESPONSE = ${TALER_DATA_HOME}/exchange/account-1.json;
+
+        PORT = 8081;
+        BASE_URL = "https://exchange.hephaistos.foo.bar/"; # TODO ensure / is present!
+        SIGNKEY_DURATION = "2 weeks";
+        SIGNKEY_LEGAL_DURATION = "2 years";
+        LOOKAHEAD_SIGN = "3 weeks 1 day";
+        KEYDIR = "\${TALER_DATA_HOME}/exchange/live-keys/";
+        REVOCATION_DIR = "\${TALER_DATA_HOME}/exchange/revocations/";
+        TERMS_ETAG = 0;
+        PRIVACY_ETAG = 0;
+      };
+    };
   };
 }
