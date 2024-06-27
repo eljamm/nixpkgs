efd92dff5dce27059748e3845c7501a4701e1316
Atemu <atemu.main@gmail.com>
2024-06-27 18:47:14 +0200

    nixos/taler: init module
    basic config set that makes the service at least start
    
    add secmod helpers and taler-global runtime dir
    
    support for includes
    
    refactor runtimeDir
    
    taler denominations
    
    Only enable services if taler is enabled
    
    fix wirewatch service name
    
    use correct permissions for database schema
    
    The current permissions don't work or aren't enough and cause the
    wirewatch and closer services to fail.
    
    exchange: create `talerEnabled` variable
    
    nixos/libeufin: init module
    
    libeufin: refactor module
    
    libeufin: add main service
    
    libeufin: put in bank attr
    
    nixos/taler: configure settings using options
    
    Works, but can be refactored further
    
    taler: refactor settings options
    
    taler: more settings refactoring
    
    trim settings defaults to the absolutely necessary ones
    
    nixos/libeufin: refactor and move to separate dir
    
    nixos/libeufin: set defaultText
    
    nixos/libeufin: use getExe
    
    nixos/libeufin-bank: move to own dir
    
    nixos/libeufin: refactor
    
    nixos/libeufin: move libeufin related config into its own config file
    
    nixos/libeufin/bank: extract dbinitServiceName into var
    
    nixos/libeufin: move script to ExecStart
    
    nixos/libeufin: fix config file name
    
    nixos/taler: refactor config file
    
    nixos/taler-exchange: grant delete to taler-exchange-aggregator
    
    Would repeatedly attempt to delete in a table where it wasn't allowed to and
    cause insane spam in the postgres log.
    
    nixos/taler/exchange: move exchange-specific options to exchange
    
    nixos/taler: move generic taler settings into taler system module
    
    nixos/taler: import exchange in module-list.nix
    
    nixos/taler-exchange: rename database
    
    nixos/taler-exchange: refactor services group name
    
    nixos/taler-exchange: undo database renaming
    
    nixos/taler-exchange: use taler-harness to generate coins
    
    The taler-wallet-cli does not have the deployment subcommand anymore,
    but the docs still say that it should be used to generate the keys.
    
    For now, the keys should be generated with taler-harness.
    
    nixos/taler: small refactors
    
    nixos/taler-exchange: add option to enable accounts
    
    nixos/taler: add missing descriptions
    
    nixos/taler(exchange): add description & use getExe'
    
    nixos/taler(merchant): init submodule
    
    use correct script for db access
    
    merchant add depositcheck path

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1350,6 +1350,9 @@
   ./services/system/uptimed.nix
   ./services/system/userborn.nix
   ./services/system/zram-generator.nix
+  ./services/taler/exchange.nix
+  ./services/taler/merchant.nix
+  ./services/taler/module.nix
   ./services/torrent/deluge.nix
   ./services/torrent/flexget.nix
   ./services/torrent/flood.nix
