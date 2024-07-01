86a4260e594321c37459e2f68ad871499b078a98
Atemu <atemu.main@gmail.com>
2024-07-01 19:41:06 +0200

    dbinit

--- nixos/modules/services/taler/exchange.nix
+++ nixos/modules/services/taler/exchange.nix
@@ -7,12 +7,21 @@
 
 let
   this = config.services.taler.exchange;
-  services = [
+  # Services that need access to the DB
+  servicesDB = [
     "httpd"
+    "aggregator"
+    "closer"
+    "wire"
+  ];
+  # Services that do not need access to the DB
+  servicesNoDB = [
     "secmod-cs"
     "secmod-eddsa"
     "secmod-rsa"
   ];
+  services = servicesDB ++ servicesNoDB;
+  dbName = "taler-exchange-httpd";
   inherit (config.services.taler) configFile;
 in
 
@@ -29,25 +38,60 @@ in
       before = [ "slices.target" ];
     };
 
-    systemd.services = lib.genAttrs (map (n: "taler-exchange-${n}") services) (name: {
-      serviceConfig = {
-        DynamicUser = true;
-        User = name;
-        Group = "taler-exchange"; # TODO refactor into constant
-        ExecStart =
-          "${this.package}/bin/${name} -c ${configFile}" + lib.optionalString this.debug " -L debug"; # TODO as a list?
-        RuntimeDirectory = name;
-        StateDirectory = name;
-        CacheDirectory = name;
-        ReadWritePaths = [ "/run/taler-system-runtime/" ];
-        # TODO more hardening
-        # PrivateTmp = "yes";
-        # PrivateDevices = "yes";
-        # ProtectSystem = "full";
-        # Slice = "taler-exchange.slice";
+    systemd.services =
+      lib.genAttrs (map (n: "taler-exchange-${n}") services) (name: {
+        serviceConfig = {
+          DynamicUser = true;
+          User = name;
+          Group = "taler-exchange"; # TODO refactor into constant
+          ExecStart =
+            "${this.package}/bin/${name} -c ${configFile}" + lib.optionalString this.debug " -L debug"; # TODO as a list?
+          RuntimeDirectory = name;
+          StateDirectory = name;
+          CacheDirectory = name;
+          ReadWritePaths = [ "/run/taler-system-runtime/" ];
+          # TODO more hardening
+          # PrivateTmp = "yes";
+          # PrivateDevices = "yes";
+          # ProtectSystem = "full";
+          # Slice = "taler-exchange.slice";
+        };
+        requires = [ "taler-exchange-dbinit.service" ];
+        after = [ "taler-exchange-dbinit.service" ];
+        wantedBy = [ "multi-user.target" ]; # TODO slice?
+      })
+      // {
+        taler-exchange-dbinit = {
+          path = [ config.services.postgresql.package ];
+          script =
+            let
+              # Taken from https://docs.taler.net/taler-exchange-manual.html#exchange-database-setup
+              # TODO generate these from servicesDB
+              dbScript = pkgs.writers.writeText "taler-exchange-db-permissions.sql" ''
+                GRANT SELECT,INSERT,UPDATE ON ALL TABLES IN SCHEMA exchange TO "taler-exchange-aggregator";
+                GRANT SELECT,INSERT,UPDATE ON ALL TABLES IN SCHEMA exchange TO "taler-exchange-closer";
+                GRANT SELECT,INSERT,UPDATE ON ALL TABLES IN SCHEMA exchange TO "taler-exchange-wire";
+                GRANT USAGE ON ALL SEQUENCES IN SCHEMA exchange TO "taler-exchange-aggregator";
+                GRANT USAGE ON ALL SEQUENCES IN SCHEMA exchange TO "taler-exchange-closer";
+                GRANT USAGE ON ALL SEQUENCES IN SCHEMA exchange TO "taler-exchange-wire";
+              '';
+            in
+            ''
+              ${this.package}/bin/taler-exchange-dbinit
+
+              psql -f ${dbScript}
+            '';
+          requires = [ "postgresql.service" ];
+          after = [ "postgresql.service" ];
+          serviceConfig = {
+            Type = "oneshot";
+            # RemainAfterExit = true;
+
+            DynamicUser = true;
+            User = "taler-exchange-httpd";
+          };
+        };
       };
-      wantedBy = [ "multi-user.target" ]; # TODO slice?
-    });
     services.taler.settings = {
       exchange = {
         # TODO these should be generated from high-level NixOS options
@@ -68,7 +112,7 @@ in
         PRIVACY_ETAG = 0;
       };
       exchangedb-postgres = {
-        CONFIG = "postgres:///taler-exchange-httpd";
+        CONFIG = "postgres:///${dbName}";
       };
       PATHS = {
         TALER_DATA_HOME = "\${STATE_DIRECTORY}/";
@@ -94,12 +138,14 @@ in
       };
     };
     services.postgresql.enable = true;
-    services.postgresql.ensureDatabases = [ "taler-exchange-httpd" ];
-    services.postgresql.ensureUsers = [
-      {
-        name = "taler-exchange-httpd";
-        ensureDBOwnership = true;
-      }
-    ];
+    services.postgresql.ensureDatabases = [ dbName ];
+    services.postgresql.ensureUsers =
+      map (service: { name = "taler-exchange-${service}"; }) servicesDB
+      ++ [
+        {
+          name = "taler-exchange-httpd";
+          ensureDBOwnership = true; # TODO clean this up
+        }
+      ];
   };
 }
