390ddd7382b653243b5485b05f1c6d7df9dffce9
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-16 18:38:19 +0100

    taler: configure settings using NixOS options
    Works, but can be refactored further

--- nixos/modules/services/taler/exchange.nix
+++ nixos/modules/services/taler/exchange.nix
@@ -127,35 +127,6 @@ in
           };
         };
       };
-    services.taler.settings = {
-      exchange = {
-        # TODO these should be generated from high-level NixOS options
-        AML_THRESHOLD = "KUDOS:1000000";
-        MAX_KEYS_CACHING = "forever";
-        DB = "postgres";
-        MASTER_PUBLIC_KEY = "Q6KCV81R9T3SC41T5FCACC2D084ACVH5A25FH44S6M5WXWZAA8P0";
-        # WIRE_RESPONSE = ${TALER_DATA_HOME}/exchange/account-1.json;
-
-        PORT = 8081; # TODO option
-        BASE_URL = "https://exchange.hephaistos.foo.bar/"; # TODO ensure / is present!
-        SIGNKEY_DURATION = "2 weeks";
-        SIGNKEY_LEGAL_DURATION = "2 years";
-        LOOKAHEAD_SIGN = "3 weeks 1 day";
-        KEYDIR = "\${TALER_DATA_HOME}/exchange/live-keys/";
-        REVOCATION_DIR = "\${TALER_DATA_HOME}/exchange/revocations/";
-        TERMS_ETAG = 0;
-        PRIVACY_ETAG = 0;
-      };
-      exchangedb-postgres = {
-        CONFIG = "postgres:///${dbName}";
-      };
-      PATHS = {
-        TALER_DATA_HOME = "\${STATE_DIRECTORY}/";
-        TALER_CACHE_HOME = "\${CACHE_DIRECTORY}/";
-        # TALER_RUNTIME_DIR = "\${RUNTIME_DIRECTORY}/";
-        TALER_RUNTIME_DIR = runtimeDir; # TODO refactor into constant
-      };
-    };
 
     users.groups.taler-exchange = { };
 
