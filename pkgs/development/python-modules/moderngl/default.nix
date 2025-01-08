ea24bcff599e4b316069f0ab81b528c3225e1578
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-08 10:20:36 +0100

    python3Packages.moderngl: use mesa platforms
    Build no longer breaks on darwin.

--- pkgs/development/python-modules/moderngl/default.nix
+++ pkgs/development/python-modules/moderngl/default.nix
@@ -7,6 +7,7 @@
   setuptools,
   glcontext,
   pythonOlder,
+  mesa,
 }:
 
 buildPythonPackage rec {
@@ -41,7 +42,6 @@ buildPythonPackage rec {
     changelog = "https://github.com/moderngl/moderngl/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ c0deaddict ];
-    # should be mesa.meta.platforms, darwin build breaks.
-    platforms = platforms.linux;
+    inherit (mesa.meta) platforms;
   };
 }
