597eda44df1ce84786a26a51955fbdcb7c28b755
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-24 12:34:43 +0200

    anki-addons: refactor

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -1,13 +1,32 @@
 {
   lib,
   anki-utils,
-  fetchFromGitHub,
-  fetchFromSourcehut,
-  nix-update-script,
-  python3,
+  newScope,
+  ...
 }:
-let
-  inherit (anki-utils) buildAnkiAddon;
-in
-{
-}
+lib.makeScope newScope (
+  self:
+  let
+    callPackage = self.newScope {
+      inherit (anki-utils)
+        buildAnkiAddon
+        buildAnkiAddonsDir
+        ;
+    };
+  in
+  {
+    adjust-sound-volume = callPackage ./adjust-sound-volume { };
+
+    anki-connect = callPackage ./anki-connect { };
+
+    local-audio-yomichan = callPackage ./local-audio-yomichan { };
+
+    passfail2 = callPackage ./passfail2 { };
+
+    recolor = callPackage ./recolor { };
+
+    reviewer-refocus-card = callPackage ./reviewer-refocus-card { };
+
+    yomichan-forvo-server = callPackage ./yomichan-forvo-server { };
+  }
+)
