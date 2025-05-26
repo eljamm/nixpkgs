465212145ad6917162536c689515dbb5c2904793
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-26 17:00:49 +0200

    Use same settings as upstream
    Will this require a warning because of removed settings options?

--- nixos/tests/taler/common/nodes.nix
+++ nixos/tests/taler/common/nodes.nix
@@ -71,6 +71,7 @@ rec {
               denominationConfig = lib.readFile ../conf/taler-denominations.conf;
               settings = {
                 exchange = {
+                  CURRENCY = "KUDOS";
                   MASTER_PUBLIC_KEY = "2TQSTPFZBC2MC4E52NHPA050YXYG02VC3AB50QESM6JX1QJEYVQ0";
                   BASE_URL = "http://exchange:8081/";
                 };
