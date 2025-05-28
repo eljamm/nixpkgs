49966c2b0e00d0e294f4898d6e191c784d044bfe
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-28 14:30:44 +0200

    merchant

--- nixos/tests/taler/common/nodes.nix
+++ nixos/tests/taler/common/nodes.nix
@@ -211,6 +211,12 @@ rec {
                 MASTER_KEY = "2TQSTPFZBC2MC4E52NHPA050YXYG02VC3AB50QESM6JX1QJEYVQ0";
                 inherit CURRENCY;
               };
+              # TODO: probably not needed, just testing
+              settings.merchant-exchange-taler = {
+                EXCHANGE_BASE_URL = "https://exchange.demo.taler.net/";
+                MASTER_KEY = "F80MFRG8HVH6R9CQ47KRFQSJP3T6DBJ4K1D9B703RJY3Z39TBMJ0";
+                CURRENCY = "KUDOS";
+              };
             };
           };
         };
