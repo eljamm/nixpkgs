fe920ef2bdcd2a1ebf9a4476c71a64a19ec4c9ec
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-03 10:03:07 +0100

    taler-sync: 1.0.0 -> 1.1.0-unstable-2025-10-30

--- pkgs/by-name/ta/taler-sync/package.nix
+++ pkgs/by-name/ta/taler-sync/package.nix
@@ -19,12 +19,25 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "taler-sync";
-  version = "1.0.0";
+  version = "1.1.0-unstable-2025-10-30";
 
   src = fetchgit {
-    url = "https://git.taler.net/sync.git";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-di+AgCCgOTvzR6Fberz1ISo6ZT+OsKikeNCU2HZHSz4=";
+    url = "https://git-www.taler.net/sync.git";
+    rev = "f541f64392bc18ea13934c6d7b93426ea42bf0a2";
+    hash = "sha256-Io0pMceV0+U/GFxkqis0SN4HU8UqlclNBs6G/y9VoQs=";
+    # Update submodules to use `git-www.taler.net` since `git.taler.net` no
+    # longer hosts source code.
+    leaveDotGit = true;
+    fetchSubmodules = false;
+    postFetch = ''
+      pushd $out
+        git reset --hard HEAD
+        substituteInPlace .gitmodules \
+          --replace-fail "git.taler.net" "git-www.taler.net"
+        git submodule update --init --recursive
+        rm -rf .git
+      popd
+    '';
   };
 
   strictDeps = true;
