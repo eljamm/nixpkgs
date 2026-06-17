98602d707e73ead5f9c031812fb817e630ba54cf
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-17 19:23:38 +0200

    wip!: python3Packages.datalab-platform: init at 1.2.1
    Assisted-By: nix-init

---
+++ pkgs/development/python-modules/datalab-platform/default.nix
@@ -0,0 +1,158 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-time
+  qt6,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  fastapi,
+  guidata,
+  numpy,
+  packaging,
+  pandas,
+  plotpy,
+  psutil,
+  pydantic,
+  pywavelets,
+  scikit-image,
+  scipy,
+  sigima,
+  uvicorn,
+
+  qtpy,
+
+  # optional-dependencies
+  babel,
+  build,
+  coverage,
+  pre-commit,
+  pylint,
+  ruff,
+  myst-parser,
+  pydata-sphinx-theme,
+  sphinx,
+  sphinx-copybutton,
+  sphinx-design,
+  sphinx-intl,
+  sphinx-sitemap,
+  sphinxcontrib-svg2pdfconverter,
+  opencv-python-headless,
+  pyinstaller,
+  pyqt6,
+  httpx,
+  pytest,
+  pytest-xvfb,
+
+  # tests
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "datalab-platform";
+  version = "1.2.1";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "DataLab-Platform";
+    repo = "DataLab";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-rJDA5qYv2LYMyrckxNy63Gqn8HYU62qG0OAioztKGtA=";
+  };
+
+  nativeBuildInputs = [
+    qt6.wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    qt6.qtbase
+  ];
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    fastapi
+    guidata
+    numpy
+    packaging
+    pandas
+    plotpy
+    psutil
+    pydantic
+    pywavelets
+    scikit-image
+    scipy
+    sigima
+    uvicorn
+
+    qtpy
+  ];
+
+  optional-dependencies = {
+    dev = [
+      babel
+      build
+      coverage
+      pre-commit
+      pylint
+      ruff
+    ];
+    doc = [
+      myst-parser
+      pydata-sphinx-theme
+      sphinx
+      sphinx-copybutton
+      sphinx-design
+      sphinx-intl
+      sphinx-sitemap
+      sphinxcontrib-svg2pdfconverter
+    ];
+    exe = [
+      opencv-python-headless
+      pyinstaller
+      pyqt6
+    ];
+    opencv = [
+      opencv-python-headless
+    ];
+    qt = [
+      pyqt6
+    ];
+    test = [
+      httpx
+      pytest
+      pytest-xvfb
+    ];
+  };
+
+  # TODO: qtpy.QtBindingsNotFoundError: No Qt bindings could be found
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonRelaxDeps = [
+    "guidata"
+    "plotpy"
+    "scipy"
+  ];
+
+  pythonImportsCheck = [
+    "datalab"
+  ];
+
+  meta = {
+    description = "Open-source Platform for Scientific and Technical Data Processing and Visualization";
+    homepage = "https://github.com/DataLab-Platform/DataLab";
+    changelog = "https://github.com/DataLab-Platform/DataLab/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ eljamm ];
+    teams = with lib.teams; [ ngi ];
+  };
+})
